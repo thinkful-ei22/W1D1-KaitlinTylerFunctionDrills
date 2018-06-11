57d3afa4a7a9c6afecc4f2139da6a1d18a96ca39
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-11 14:47:29 -0400

    JS Problem 4

--- JSDrills1.js
+++ JSDrills1.js
@@ -4,6 +4,8 @@ console.log('Hi, my name is Chris and I\'m 29 years old');
 
 //2-8.
 const whoAmI = function(name,age) {
+    let yearOfBirth = 2018 - age;
     console.log('Hi, my name is Chris and I\'m 29 years old');
+    console.log(`I was born in ${yearOfBirth}.`);
 }
 whoAmI('Chris', '29');

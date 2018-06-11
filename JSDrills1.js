53df0386f01fb04e4d58d3612ff5c10bca91b63d
Tyler Warrick <tcw5038@gmail.com>
2018-06-11 11:44:09 -0700

    Updated exercises

--- JSDrills1.js
+++ JSDrills1.js
@@ -1,9 +1,9 @@
 'use strict';
 //1.
 console.log('Hi, my name is Chris and I\'m 29 years old');
-//2.
-const whoAmI = function() {
+
+//2-8.
+const whoAmI = function(name,age) {
     console.log('Hi, my name is Chris and I\'m 29 years old');
 }
-
-whoAmI();
+whoAmI('Chris', '29');

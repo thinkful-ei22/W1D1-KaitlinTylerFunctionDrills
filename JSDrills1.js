63411d652b38dfeea1ebd06b17408b3a78e64329
Tyler Warrick <tcw5038@gmail.com>
2018-06-11 11:52:10 -0700

    Next exercise

--- JSDrills1.js
+++ JSDrills1.js
@@ -3,9 +3,14 @@
 console.log('Hi, my name is Chris and I\'m 29 years old');
 
 //2-8.
+
+const yearOfBirth = function(age)
+{
+    return (2018 - age);
+}
 const whoAmI = function(name,age) {
-    let yearOfBirth = 2018 - age;
+    var yob = yearOfBirth(age);
     console.log('Hi, my name is Chris and I\'m 29 years old');
-    console.log(`I was born in ${yearOfBirth}.`);
+    console.log(`I was born in ${yob}.`);
 }
 whoAmI('Chris', '29');

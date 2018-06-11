14ace8608a13f9114159a4cef290e75a6cb94bd1
Kaitlin Bunn <kabunn1991@gmail.com>
2018-06-11 15:01:42 -0400

    Drill 6 added

--- JSDrills1.js
+++ JSDrills1.js
@@ -4,10 +4,22 @@ console.log('Hi, my name is Chris and I\'m 29 years old');
 
 //2-8.
 
-const yearOfBirth = function(age)
-{
-    return (2018 - age);
+const yearOfBirth = function(age) {
+    if (age >= 0) {
+        return (2018 - age);
+    }
+    if (age < 0 ) {
+        throw new Error('Age can not be negative');
+    }
+    try {
+        yearOfBirth(-1); // Throw error function
+    }
+    catch(e) {
+        console.error(e.message);
+    }
 }
+
+
 const whoAmI = function(name,age) {
     var yob = yearOfBirth(age);
     console.log('Hi, my name is Chris and I\'m 29 years old');

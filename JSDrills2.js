49382d2be9e057286d0216c7720076dd20e03526
Tyler Warrick <tcw5038@gmail.com>
2018-06-11 12:41:44 -0700

    To infinity and beyond

--- JSDrills2.js
+++ JSDrills2.js
@@ -8,3 +8,31 @@ const jediName = function(firstName, lastName) {
 }
 
 jediName('Darth', 'Vader');
+
+//2. To infinity
+const beyond = function(num)
+{
+   /* if (num === Infinity || Math.log(0))
+    {
+        console.log('And beyond');
+    }*/
+    if(!isFinite(num))
+    {
+        console.log('And beyond');
+    }
+    else if(num !== Infinity && num > 0)
+    {
+        console.log('To infinity');
+    }
+    else if(num !== Infinity && num < 0)
+    {
+        console.log('To negative infinity');
+    }
+    else if (num === 0)
+    {
+        console.log('Staying home');
+    }
+
+}
+
+beyond();
